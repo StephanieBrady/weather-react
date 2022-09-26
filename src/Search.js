c902e8d705aa507b38c516429871cb1037552f24
StephanieBrady <stephaniebrady1929@gmail.com>
2022-09-26 15:28:50 -0400

    moved code from sandbox to vscode

---
+++ src/Search.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <form className="form-inline">
+      <div className="input-group">
+        <input
+          type="text"
+          id="search-form"
+          className="form-control search-form"
+          placeholder="Enter City"
+        />
+        <span className="input-group-btn">
+          <button
+            id="search-this"
+            type="submit"
+            className="pull-right btn btn-default search-btn"
+          >
+            <i className="fa fa-search"></i>
+          </button>
+        </span>
+      </div>
+      <button
+        id="currentLocationButton"
+        type="submit"
+        className="currentLocationButton"
+      >
+        Current Location
+      </button>
+    </form>
+  );
+}
---
+++ src/Weather.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Search from "./Search";
+export default function Weather() {
+  let weatherData = {
+    city: "Fort Payne",
+    temperature: 80,
+    date: "Tuesday, September 6",
+    description: "Sunny",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 6,
+  };
+
+  return (
+    <div className="weather">
+      <div className="weatherapp text-center">
+        <Search />
+        <h1>{weatherData.city}</h1>
+        <h3>
+          {" "}
+          {weatherData.temperature}°F
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+          </div>
+        </h3>
+        <h4>
+          {weatherData.date}
+          <br />
+          {weatherData.time}
+        </h4>
+        <h5>
+          {weatherData.description} <br />
+          {weatherData.wind} mph <br />
+          {weatherData.humidity}% Humidity <br />
+        </h5>
+        <br /> <br />
+        <p class="gitHub-link">
+          <i class="fa-solid fa-mountain-sun"></i>
+          <a
+            href="https://github.com/StephanieBrady/weather-application"
+            class="code-id"
+          >
+            {" "}
+            Open-source code
+          </a>
+          by Stephanie Brady
+        </p>
+      </div>
+    </div>
+  );
+}
