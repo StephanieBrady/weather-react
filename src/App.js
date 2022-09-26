043db8b7bed2ca3b483dbe36364bf066ad1dd55c
StephanieBrady <stephaniebrady1929@gmail.com>
2022-09-26 15:49:48 -0400

    added index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 import "./styles.css";
--- src/Search.js
+++ src/Search.js
@@ -2,10 +2,31 @@ import React from "react";
 
 export default function Search() {
   return (
-    <form>
-      <input type="search" placeholder="Enter a city.." />
-      <button type="Submit">Search</button>
-      <button>Current Location</button>
+    <form className="form-inline">
+      <div className="input-group">
+        <input
+          type="text"
+          id="search-form"
+          className="form-control search-form"
+          placeholder="Enter City"
+        />
+        <span className="input-group-btn">
+          <button
+            id="search-this"
+            type="submit"
+            className="pull-right btn btn-default search-btn"
+          >
+            <i className="fa fa-search"></i>
+          </button>
+        </span>
+      </div>
+      <button
+        id="currentLocationButton"
+        type="submit"
+        className="currentLocationButton"
+      >
+        Current Location
+      </button>
     </form>
   );
 }
