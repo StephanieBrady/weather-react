2114c72fb4c9ec7d4cd65f9da9ee6e521ce498aa
StephanieBrady <stephaniebrady1929@gmail.com>
2022-10-05 16:12:50 -0400

    updated formatted date

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 
 import WeatherInfo from "./WeatherInfo";
 
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,40 +3,46 @@ import FormattedDate from "./FormattedDate";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="WeatherInfo">
-      <h1 className="mt-5">
-        {" "}
-        <div className="clearfix weather-temperature">
-          <img
-            src={props.weatherData.imgUrl}
-            alt={props.weatherData.description}
-            className="float-left"
-          />{" "}
-          {props.weatherData.city}
+    <div className="weather">
+      <div className="container">
+        <div className="weatherapp text-center mt-5">
+          <div className="WeatherInfo">
+            <h1 className="mt-5">
+              {" "}
+              <div className="clearfix weather-temperature">
+                <img
+                  src={props.weatherData.imgUrl}
+                  alt={props.weatherData.description}
+                  className="float-left"
+                />{" "}
+                {props.weatherData.city}
+              </div>
+            </h1>
+            <h1 className> {props.weatherData.temperature}°C</h1>
+            <h5 className="text-capitalize">
+              {props.weatherData.description} <br />
+              {props.weatherData.wind} km/h
+              <br />
+              {props.weatherData.humidity}% Humidity <br />
+            </h5>
+            <h4>
+              <FormattedDate date={props.weatherData.data} />
+            </h4>
+            <br /> <br />
+            <p class="gitHub-link">
+              <i class="fa-solid fa-mountain-sun"></i>
+              <a
+                href="https://github.com/StephanieBrady/weather-react"
+                class="code-id"
+              >
+                {" "}
+                Open-source code
+              </a>{" "}
+              by Stephanie Brady
+            </p>
+          </div>
         </div>
-      </h1>
-      <h1 className> {props.weatherData.temperature}°C</h1>
-      <h5 className="text-capitalize">
-        {props.weatherData.description} <br />
-        {props.weatherData.wind} km/h
-        <br />
-        {props.weatherData.humidity}% Humidity <br />
-      </h5>
-      <h4>
-        <FormattedDate date={props.weatherData.data} />
-      </h4>
-      <br /> <br />
-      <p class="gitHub-link">
-        <i class="fa-solid fa-mountain-sun"></i>
-        <a
-          href="https://github.com/StephanieBrady/weather-react"
-          class="code-id"
-        >
-          {" "}
-          Open-source code
-        </a>{" "}
-        by Stephanie Brady
-      </p>
+      </div>
     </div>
   );
 }
