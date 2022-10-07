4e0fc8122225f065521c5fcf18852b364c23e59b
StephanieBrady <stephaniebrady1929@gmail.com>
2022-10-07 10:35:35 -0400

    updated search css

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#143E53"
+      color="#3D5565"
       size={props.size}
       animate={true}
     />
